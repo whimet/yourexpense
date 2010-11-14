b8b6fda3717661c3495a0a26e059e3678fd1965f
whimet <whimet@gmail.com>
2010-11-14 13:08:09 +0800

    removed user getter from Expense

--- java/src/main/java/yourexpense/domain/Expense.java
+++ java/src/main/java/yourexpense/domain/Expense.java
@@ -62,10 +62,6 @@ public class Expense {
         this.date = date;
     }
 
-    public User getUser() {
-        return user;
-    }
-
     public void setUser(User user) {
         this.user = user;
     }
